1787fed170ed96aea0c7b77d544ffb5205bd1336
Higor <higorpiresoliveira@gmail.com>
2021-12-14 16:56:02 -0300

    :hammer: add target in footer links

--- src/partials/Footer/index.tsx
+++ src/partials/Footer/index.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC = () => {
         <footer id="page-footer">
             <img id="logo" src={logo} alt="Logotipo da 3dschool" />
             <p>
-                *Imagens, ícones e vetores usados no site foram inspirados ou retirados através dos sites <a href="https://pixabay.com">Pixabay</a>, <a href="https://www.freepik.com/">Freepik</a> e <a href="https://www.flaticon.com/">Flaticon</a>.
+                *Imagens, ícones e vetores usados no site foram inspirados ou retirados através dos sites <a target="_blank" href="https://pixabay.com">Pixabay</a>, <a target="_blank" href="https://www.freepik.com/">Freepik</a> e <a target="_blank" href="https://www.flaticon.com/">Flaticon</a>.
             </p>
             <p>**não consideramos nessa conta o custo do aparelho celular (que na maioria dos casos será do próprio aluno) nem logística de transporte.</p>
         </footer>
