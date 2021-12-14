f3d890bbbe7d9fbdd1669b783f99d0953c96ccbb
Higor <higorpiresoliveira@gmail.com>
2021-12-14 16:53:24 -0300

    :hammer: remove disclaimer from section to footer

--- src/partials/HowWeDoThis/index.tsx
+++ src/partials/HowWeDoThis/index.tsx
@@ -12,10 +12,8 @@ const HowWillWeDoThis: React.FC = () => {
         <section id="how-will-we-do-this">
             <Title>Como vamos fazer isso?</Title>
             <Paragraph>
-                Vamos fornecer para cada escola participante kits que contém um óculos de realidade virtual a ser usado junto com um aparelho celular e um controle VR bluetooth. Através de um aplicativo disponibilizado no celular do aluno, e em conjunto com os aparelhos mencionados, nós conseguiremos criar diferentes ambientes virtuais para auxiliar no ensino de crianças, adolescentes e adultos. Além disso, nosso objetivo é democratizar o acesso a essas tecnologias, e por isso, trabalhamos duro para diminuir os custos de implantação da tecnologia, sendo assim prometemos a implantação da nossa solução por um custo de até <strong>R$40 reais por kit*</strong>, que pode ser compartilhado com mais de um aluno.
+                Vamos fornecer para cada escola participante kits que contém um óculos de realidade virtual a ser usado junto com um aparelho celular e um controle VR bluetooth. Através de um aplicativo disponibilizado no celular do aluno, e em conjunto com os aparelhos mencionados, nós conseguiremos criar diferentes ambientes virtuais para auxiliar no ensino de crianças, adolescentes e adultos. Além disso, nosso objetivo é democratizar o acesso a essas tecnologias, e por isso, trabalhamos duro para diminuir os custos de implantação da tecnologia, sendo assim prometemos a implantação da nossa solução por um custo de até <strong>R$40 reais por kit**</strong>, que pode ser compartilhado com mais de um aluno.
             </Paragraph>
-
-            <span>*não consideramos nessa conta o custo do aparelho celular (que na maioria dos casos será do próprio aluno) nem logística de transporte.</span>
 
             <img src={image} alt="Mulher usando óculos de realidade virtual" />
         </section>
