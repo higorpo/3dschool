832b97cb79b610d728690b6b31acb8f57a9ef883
Higor <higorpiresoliveira@gmail.com>
2021-12-14 16:43:58 -0300

    :sparkles: add footer details

--- src/partials/Footer/index.tsx
+++ src/partials/Footer/index.tsx
@@ -8,6 +8,11 @@ const Footer: React.FC = () => {
     return (
         <footer id="page-footer">
             <img id="logo" src={logo} alt="Logotipo da 3dschool" />
+            <p>
+                Imagens, ícones e vetores usados no site foram inspirados ou retirados através dos sites <a href="https://pixabay.com">Pixabay</a>, <a href="https://www.freepik.com/">Freepik</a> e <a href="https://www.flaticon.com/">Flaticon</a>.
+                <br />
+                Site criado para o desafio da <a href="https://rocketseat.com.br">Rocketseat</a>.
+            </p>
         </footer>
     );
 }
